db389d04cc82dc514abc4d1124d4339cbb02f04d
Mohamed Emad <m.emad550@gmail.com>
2018-06-30 00:36:44 +0200

    add second image with the same click counter

--- plugin.js
+++ plugin.js
@@ -1,8 +1,26 @@
-let cat = document.querySelector('img'),
-    clicksContainer = document.querySelector('.clicksContainer');
-    clicks = 0;
-    clicksContainer.innerHTML = clicks;
-cat.addEventListener('click', function(){
-    clicks ++;
-    clicksContainer.innerHTML = clicks;
-});
+let cats = document.querySelectorAll('.cat'),
+    clicksContainer = document.querySelector('.clicksContainer'),
+    clicks = 0,
+    names = document.querySelectorAll('.name'),
+    catsArray = [
+        './images/2368516302_982f046bb5_o.jpg',
+        './images/5372754294_db6acaa1e5_b.jpg',
+    ],
+    catsNames = [
+        'Cat 1',
+        'Cat 2'
+    ]
+;
+
+clicksContainer.innerHTML = clicks;
+for (let i = 0; i < cats.length; i++) {
+    cats[i].setAttribute("src", catsArray[i]);
+    cats[i].addEventListener('click', () => {
+        clicks ++;
+        clicksContainer.innerHTML = clicks;
+    });
+    for (let i = 0; i < catsNames.length; i++) {
+        names[i].innerHTML = catsNames[i]
+    }
+}
+
