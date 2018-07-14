8689186085a8d98789864a1f3f5065aa51dac3c9
Mohamed Emad <m.emad550@gmail.com>
2018-07-14 17:37:08 +0200

    make each cat has own counter

--- plugin.js
+++ plugin.js
@@ -1,83 +1,45 @@
-    class Cat {
-        constructor(name, src, counter){
-            this.name = name;
-            this.src = src;
-            this.counter = counter;
-            this.container = `
-                <div class="container">
-                    <img src='${this.src}' class ='clicker' alt='${this.name}' title='${this.name}'>
-                    <h1> ${this.name} </h1> 
-                    <span class="clickContainer">${this.counter}</span>
-                </div>
-            `;
-            this.display();
-        }
-        display() {
-            let body = document.querySelector('body');
-            body.innerHTML += `${this.container}`
-            this.click();
-        }
-        click(){
-            let imgs = document.querySelectorAll('.clicker');
-            console.log(imgs.length);
-            for (let i = 0; i < imgs.length; i++){
-                let img = imgs[i];
-                img.addEventListener('click',() =>{
-                    this.counter ++;
-                    const counters = document.querySelectorAll('.clickContainer');
-                    for(let i = 0; i < counters.length; i++) {
-                        counters[i].innerHTML = this.counter;
-                    }
-                    console.log(this.counter);
-                });
-            }
-            
+class Cat {
+    constructor(name, src, counter){
+        this.name = name;
+        this.src = src;
+        this.counter = counter;
+        this.container = `
+            <div class="container">
+                <img src='${this.src}' class ='clicker' alt='${this.name}' title='${this.name}'>
+                <h1> ${this.name} </h1> 
+                <span class="clickContainer">${this.counter}</span>
+            </div>
+        `;
+        this.display();
+    }
+    display() {
+        let body = document.querySelector('body');
+        body.innerHTML += `${this.container}`
+        this.click();
+    }
+    click() {
+        let imgs = document.querySelectorAll('.clicker');
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].addEventListener('click',() =>{                
+                let clicks = document.querySelectorAll('.clickContainer');
+                clicks[i].innerHTML++;
+            });
         }
     }
+}
 
-
-
-    const cats = [
-        {
-            catName: 'Kitty',
-            catUrl: './images/1.jpg',
-            catCounter: 0
-        },
-        {
-            catName: 'Moala',
-            catUrl: './images/2.jpg',
-            catCounter: 0
-        }
-    ]
-    for (let i = 0; i < cats.length; i++) {
-        new Cat(cats[i]['catName'], cats[i]['catUrl'], cats[i]['catCounter']);
+const cats = [
+    {
+        catName: 'Kitty',
+        catUrl: './images/1.jpg',
+        catCounter: 0
+    },
+    {
+        catName: 'Moala',
+        catUrl: './images/2.jpg',
+        catCounter: 0
     }
-
-    // // clear the screen for testing
-    // document.body.innerHTML = '';
-    // document.body.style.background="white";
-
-    // var nums = [1,2,3];
-
-    // // Let's loop over the numbers in our array
-    // for (var i = 0; i < nums.length; i++) {
-
-    //     // This is the number we're on...
-    //     var num = nums[i];
-
-    //     // We're creating a DOM element for the number
-    //     var elem = document.createElement('div');
-    //     elem.textContent = num;
-
-    //     // ... and when we click, alert the value of `num`
-    //     elem.addEventListener('click', (function(num) {
-    //         return function(){
-    //             alert(num);
-    //         }
-            
-    //     })(num));
-
-    //     // finally, let's add this element to the document
-    //     document.body.appendChild(elem);
-    // };
-
+]
+for (let i = 0; i < cats.length; i++) {
+    new Cat(cats[i]['catName'], cats[i]['catUrl'], cats[i]['catCounter']);
+}
